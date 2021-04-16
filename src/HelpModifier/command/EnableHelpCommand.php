b82d3c5db2dc3651160f3938d82952b65a01af6f
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2021-04-15 17:30:37 -0700

    Create EnableHelpCommand.php

---
+++ src/HelpModifier/command/EnableHelpCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace HelpModifier\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginIdentifiableCommand;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+use pocketmine\Player;
+use HelpModifier\HelpModifier;
+
+class EnableHelpCommand extends Command implements PluginIdentifiableCommand {
+
+    private $plugin;
+
+    public function __construct(HelpModifier $plugin) {
+        $this->plugin = $plugin;
+        parent::__construct(
+            "enablehelp",
+            "Enables help",
+            "/enablehelp"
+        );
+        $this->setPermission("help.enable");
+    }
+
+    public function getPlugin() : Plugin {
+        return $this->plugin;
+    }
+
+    public function execute(CommandSender $sender, string $label, array $args) {
+        if (!$this->testPermission($sender)) {
+            return;
+        }
+
+        if ($this->plugin->getConfig()->get("enable-help") == true) {
+            $sender->sendMessage(TextFormat::RED . "Help is already enabled");
+        } else if ($this->plugin->getConfig()->get("enable-help") == false) {
+            $this->plugin->getConfig()->set("enable-help", true);
+            $this->plugin->getConfig()->save();
+            $sender->sendMessage(TextFormat::GREEN . "Help has been enabled");
+        }
+    }
+
+}
